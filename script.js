71ac9538a92c8ea7afe1452062632456753a50fa
Randall Burgess <randallallenburgess@gmail.com>
2019-12-10 14:10:05 -0800

    first commit HW3

---
+++ script.js
@@ -0,0 +1,13 @@
+function generate(length = 8){
+
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "1234567890";
+    var symbols = "!@#$%^&*()_+";
+
+    var all  = uppercase + lowercase + numbers + symbols;
+
+    var password = "";
+
+    
+}
