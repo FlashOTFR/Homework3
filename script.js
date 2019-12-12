dbb125c4f02ea5bb29502d3c4bee27f7e81cd6fa
Randall Burgess <randallallenburgess@gmail.com>
2019-12-11 21:34:27 -0800

    objects built, random character functions built. working on building event functions next

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-function generate(length = 8){
+var result = document.querySelector('#password');
 
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var generate = document.querySelector('#generate')
+
+var copyClip = document.querySelector('#copyClip')
+
+
+
+var randomChoice = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    numbers: getRandomNumber,
+    symbols: getRandomSymbol
+};
+
+generate.addEventListener('click', )
+
+function getRandomLower() {
+    
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = "1234567890";
-    var symbols = "!@#$%^&*()_+";
+    return lowercase[Math.floor(Math.random() * lowercase.length)];
+}
 
-    var all  = uppercase + lowercase + numbers + symbols;
-
-    var password = "";
-
+function getRandomUpper() {
+    
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return uppercase[Math.floor(Math.random() * uppercase.length)];
     
 }
+
+function getRandomNumber() {
+
+    var numbers = "1234567890";
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomSymbol() {
+
+    var symbols = "!@#$%^&*()_+";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+
+}
+
+
+
+
+
+
+
+console.log(getRandomLower());
+console.log(getRandomNumber());
+console.log(getRandomUpper());
+console.log(getRandomSymbol());
